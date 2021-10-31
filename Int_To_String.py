0f47b9b85aadc6b8fa98ff12b8f4eada3729e57d
MITESHDABHI <dabhimitesh18@gmail.com>
2021-10-31 21:08:51 +0530

    Integer TO String Converter

---
+++ Int_To_String.py
@@ -0,0 +1,11 @@
+n = input("Enter The Number")
+# check  and print type of num variable  
+print(type(n))  
+print(n)  
+  
+# convert the num into string  
+con_num = str(n)  
+  
+# check  and print type converted_num variable  
+print(type(con_num))  
+print(con_num)  
